5a18ea64fbf3abd1ca089bc408649bdf6121dbf5
Strox0 <molnartamas332@gmail.com>
2024-04-23 16:51:17 +0200

    fixed premake setup file

--- setup.lua
+++ setup.lua
@@ -25,20 +25,10 @@ project "ImageExtract"
 	kind "WindowedApp"
 	language "C++"
 	cppdialect "C++20"
-	staticruntime "off"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/")
 	objdir ("bin-int/" .. outputdir .. "/")
-
-	files {
-		"src/**.cpp",
-		"src/**.h",
-		"src/**.c"
-	}
-
-	libdirs {
-		"vendor/opencv/lib"
-	}
 
 	includedirs {
 		"vendor/IMAF/IMAF/src",
@@ -46,6 +36,74 @@ project "ImageExtract"
 		"vendor/imgui",
 		"vendor/opencv/include"
 	}
+
+	files {
+		"src/**.cpp",
+		"src/**.h",
+		"src/**.c"
+	}
+
+	filter "configurations:Debug"
+			libdirs {
+				"vendor/opencv/libs/Debug"
+			}
+      links {
+      	"gif.lib",
+				"jpeg.lib",
+				"libpng16d.lib",
+				"libsharpyuv.lib",
+				"libwebp.lib",
+				"lzma.lib",
+				"opencv_calib3d4d.lib",
+				"opencv_core4d.lib",
+				"opencv_dnn4d.lib",
+				"opencv_features2d4d.lib",
+				"opencv_flann4d.lib",
+				"opencv_highgui4d.lib",
+				"opencv_imgcodecs4d.lib",
+				"opencv_imgproc4d.lib",
+				"opencv_ml4d.lib",
+				"opencv_objdetect4d.lib",
+				"opencv_photo4d.lib",
+				"opencv_stitching4d.lib",
+				"opencv_video4d.lib",
+				"opencv_videoio4d.lib",
+				"openjp2.lib",
+				"tiffd.lib",
+				"turbojpeg.lib",
+				"zlibd.lib"
+      }
+
+	filter "configurations:Release"
+			libdirs {
+				"vendor/opencv/libs/Release"
+			}
+      links {
+      	"gif.lib",
+				"jpeg.lib",
+				"libpng16.lib",
+				"libsharpyuv.lib",
+				"libwebp.lib",
+				"lzma.lib",
+				"opencv_calib3d4.lib",
+				"opencv_core4.lib",
+				"opencv_dnn4.lib",
+				"opencv_features2d4.lib",
+				"opencv_flann4.lib",
+				"opencv_highgui4.lib",
+				"opencv_imgcodecs4.lib",
+				"opencv_imgproc4.lib",
+				"opencv_ml4.lib",
+				"opencv_objdetect4.lib",
+				"opencv_photo4.lib",
+				"opencv_stitching4.lib",
+				"opencv_video4.lib",
+				"opencv_videoio4.lib",
+				"openjp2.lib",
+				"tiff.lib",
+				"turbojpeg.lib",
+				"zlib.lib"
+      }
 
 	links {
 		"IMAF"
