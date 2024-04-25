45ea7c045e7b7169d738e9f6d473cba550d4cb6e
Strox0 <molnartamas332@gmail.com>
2024-04-25 20:12:41 +0200

    Application init added

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,22 @@
-#include "opencv2/opencv.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgcodecs.hpp"
-
+#include "IMAF/Application.h"
 #include <Windows.h>
+
+#include "GUI.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	cv::Mat img = cv::imread("./test.jpg");
-	cv::imshow("TEST", img);
-	cv::waitKey(0);
+	IMAF::AppProperties props;
+	props.resizeable = true;
+	props.name = "Image Extractor";
+
+	IMAF::Application* app = new IMAF::Application(props);
+
+	std::shared_ptr<GUI> ptr = std::make_shared<GUI>();
+	app->AddPanel(ptr);
+	
+	app->Run();
+
+	delete app;
 
 	return 0;
 }
